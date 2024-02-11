5a654e33a109fe03e7927d972413ef208f2bf0c5
Calvin Mann <calvinmann@gmail.com>
2024-02-11 15:41:20 +0100

    Added service context export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Provider } from './provider.js';
 export { ProviderContext } from './provider-context.js';
 export { EnvironmentContext } from './environment-context.js';
 export { FeatureContext } from './feature-context.js';
+export { ServiceContext } from './service-context.js';
 export { CachePolicy, GlobalCachePolicy, LruCachePolicy, TimedCachePolicy } from './cache/index.js';
 export type { ProviderConfig, ProviderLoader, FullDisposable as Disposable } from './type.js';
